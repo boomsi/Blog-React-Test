b687c47aae18cdb82f2d0a61f9ae305b35a91cf1
boomsi <libo19431@163.com>
2019-10-01 12:30:33 +0800

    init Project

---
+++ src/pages/Show/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.less';
+import { Row, Col } from 'antd'
+
+function App() {
+  return (
+    <div className="container">
+      content
+    </div>
+  )
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './index.less';
+import { Row, Col, Menu, Input } from 'antd'
+import Show from './Show'
+
+const { Search } = Input
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      current: ['home', 'life', 'message', 'user']
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <header className='header'>
+          <Row type='flex' align='middle'>
+            <Col span={14} offset={2}>
+              <Menu defaultSelectedKeys={['home']} mode="horizontal" theme='light'>
+                <Menu.Item key="home">
+                  Home
+                </Menu.Item>
+                <Menu.Item key="life">
+                  LIFE
+                </Menu.Item>
+                <Menu.Item key="message">
+                  MESSAGE
+                </Menu.Item>
+                <Menu.Item key="user">
+                  USER
+                </Menu.Item>
+              </Menu>
+            </Col>
+            <Col span={8}>
+            <Search
+              placeholder="input search text"
+              onSearch={value => console.log(value)}
+              style={{ width: 200 }}
+            />
+            </Col>
+          </Row>
+        </header>
+        <div className='body'>
+          <Show />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
